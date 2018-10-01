e81e03bf828edf1e96506a79fe4d235fb7ec9fae
Krzysiekkwiatkowski <krzyszt.kwiatkowski@gmail.com>
2018-10-01 21:57:30 +0200

    Done some changes to finish program properly

--- src/main/java/pl/coderslab/Day3/Browser.java
+++ src/main/java/pl/coderslab/Day3/Browser.java
@@ -17,9 +17,10 @@ import java.util.Set;
 @WebFilter(filterName = "Browser", urlPatterns = "/*")
 public class Browser implements Filter {
 
-    private String ADD_QUERY = "INSERT INTO browser(name, date, time) VALUES (?,?,?)";
+    private String ADD_QUERY = "INSERT INTO browser(name, date, parameters,time) VALUES (?,?,?,?)";
     private String browser;
     private String date;
+    private String parameters;
     private int time;
 
     public void destroy() {
@@ -30,10 +31,16 @@ public class Browser implements Filter {
             Map<String, String> map = new HashMap<String, String>();
             HttpServletRequest httpServletRequest = (HttpServletRequest) req;
             Enumeration headerNames = ((HttpServletRequest) req).getHeaderNames();
+            Enumeration<String> params = req.getParameterNames();
             while (headerNames.hasMoreElements()) {
                 String key = (String) headerNames.nextElement();
                 String value = httpServletRequest.getHeader(key);
                 map.put(key, value);
+            }
+            while (params.hasMoreElements()){
+                String element = params.nextElement();
+                String[] elem = req.getParameterValues(element);
+                parameters = element + " - " + elem[0] + "; ";
             }
             Set<String> keySet = map.keySet();
             for (String key : keySet) {
@@ -50,7 +57,8 @@ public class Browser implements Filter {
             PreparedStatement preparedStatement = connection.prepareStatement(ADD_QUERY);
             preparedStatement.setString(1, browser);
             preparedStatement.setString(2, date);
-            preparedStatement.setInt(3, time);
+            preparedStatement.setString(3, parameters);
+            preparedStatement.setInt(4, time);
             preparedStatement.executeUpdate();
         } catch (SQLException e){
             e.printStackTrace();
