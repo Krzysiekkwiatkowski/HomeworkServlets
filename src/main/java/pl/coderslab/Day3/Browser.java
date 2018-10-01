8b48891728443fb57b3dd242afeb59ceb1705123
Krzysiekkwiatkowski <krzyszt.kwiatkowski@gmail.com>
2018-10-01 21:11:22 +0200

    Last filter exercise to do

---
+++ src/main/java/pl/coderslab/Day3/Browser.java
@@ -0,0 +1,64 @@
+package pl.coderslab.Day3;
+
+import com.mysql.jdbc.Connection;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.time.LocalDateTime;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+@WebFilter(filterName = "Browser", urlPatterns = "/*")
+public class Browser implements Filter {
+
+    private String ADD_QUERY = "INSERT INTO browser(name, date, time) VALUES (?,?,?)";
+    private String browser;
+    private String date;
+    private int time;
+
+    public void destroy() {
+    }
+
+    public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws ServletException, IOException {
+        try {
+            Map<String, String> map = new HashMap<String, String>();
+            HttpServletRequest httpServletRequest = (HttpServletRequest) req;
+            Enumeration headerNames = ((HttpServletRequest) req).getHeaderNames();
+            while (headerNames.hasMoreElements()) {
+                String key = (String) headerNames.nextElement();
+                String value = httpServletRequest.getHeader(key);
+                map.put(key, value);
+            }
+            Set<String> keySet = map.keySet();
+            for (String key : keySet) {
+                if (key.equals("user-agent")) {
+                    browser = map.get(key);
+                }
+            }
+            long startTime = System.currentTimeMillis();
+            chain.doFilter(req, resp);
+            long stopTime = System.currentTimeMillis();
+            date = LocalDateTime.now().toString();
+            time = (int)(stopTime - startTime);
+            java.sql.Connection connection = DBFilter.getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement(ADD_QUERY);
+            preparedStatement.setString(1, browser);
+            preparedStatement.setString(2, date);
+            preparedStatement.setInt(3, time);
+            preparedStatement.executeUpdate();
+        } catch (SQLException e){
+            e.printStackTrace();
+        }
+    }
+
+    public void init(FilterConfig config) throws ServletException {
+
+    }
+
+}
---
+++ src/main/java/pl/coderslab/Day3/DBFilter.java
@@ -0,0 +1,20 @@
+package pl.coderslab.Day3;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBFilter {
+    private static String DB_URL = "jdbc:mysql://localhost:3306/filter?useSSL=false&useUnicode=yes&characterEncoding=UTF-8";
+    private static String DB_USER = "root";
+    private static String DB_PASSWORD = "coderslab";
+
+    public static Connection getConnection() throws SQLException {
+        try{
+            Class.forName("com.mysql.jdbc.Driver").newInstance();
+        } catch (Exception e){
+            return DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
+        }
+        return DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
+    }
+}
